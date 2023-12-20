82d2a65db335adad27763827c384ec23a4b8e0e4
wisely <no1piece@outlook.com>
2023-12-21 02:38:19 +0800

    manual set sequence

--- injbot.js
+++ injbot.js
@@ -9,9 +9,11 @@ const {
 }= require('@injectivelabs/sdk-ts');
 const { MsgSend } = require('@injectivelabs/sdk-ts');
 const { BigNumberInBase, DEFAULT_STD_FEE } = require('@injectivelabs/utils');
+const { program } = require('commander');
+
 
 /** MsgSend Example */
-const sendTask = async () => {
+const sendTask = async (sequence) => {
   const network = getNetworkInfo(Network.Testnet)
   const privateKeyHash = process.env.PRIVATE_KEY;
   const privateKey = PrivateKey.fromHex(privateKeyHash)
@@ -35,7 +37,7 @@ const sendTask = async () => {
     dstInjectiveAddress: injectiveAddress,
   })
 
-  const sequence_id =parseInt(accountDetails.account.base_account.sequence,10);
+  const sequence_id =sequence;
   const accountNumber =parseInt(accountDetails.account.base_account.account_number,10,);
   console.log("sequence id:",sequence_id);
   console.log("accountNum:",accountNumber);
@@ -47,7 +49,7 @@ const sendTask = async () => {
     memo: process.env.MEMO,
     fee: DEFAULT_STD_FEE,
     pubKey: publicKey,
-    sequence: sequence_id+1,
+    sequence: sequence_id,
     accountNumber: parseInt(
       accountDetails.account.base_account.account_number,
       10,
@@ -81,8 +83,20 @@ const sendTask = async () => {
 
 }
 
-sendTask().then((result)=>{
+program
+.name("mint injs bot")
+.version("0.0.2");
+
+program.action("inj")
+.argument('<number>',"start sequence")
+.action((args)=>{
+  const sequence=parseInt(args);
+  sendTask(sequence);
+})
+
+sendTask(sequence).then((result)=>{
   console.log("Hello world");
   console.log("Hash:",result);
 })
+program.parse();
 console.log("Hello injs");
